4b8ed9ff4f37b67415f08813962c4b5cb9ed601f
JohanEstebanM <morenojimenezjohanesteban@gmail.com>
2026-08-11 03:52:53 -0500

    first change

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,9 @@ export default function Home() {
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
             Prueba de pagina{" "}
           </h1>
+          <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
+            primer cambio
+          </p>
         </div>
       </main>
     </div>
